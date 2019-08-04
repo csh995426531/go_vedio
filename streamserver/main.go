2047867022c91a71e4c9e148569e19d1bcc80937
csh995426531 <1114313879@qq.com>
2019-08-05 07:41:44 +0800

    stream 完成

--- streamserver/main.go
+++ streamserver/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main(){
-	
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type middleWareHander struct {
+	r *httprouter.Router
+	l *ConnLimiter
+}
+
+// NewMiddleWareHander 中间处理
+func NewMiddleWareHander(r *httprouter.Router, cc int) http.Handler {
+	m := middleWareHander{}
+	m.r = r
+	m.l = NewConnLimiter(cc)
+	return m
+}
+
+// RegisterHandlers 注册处理
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/vidoes//:vid-id", streamHandler)
+
+	router.POST("/vidoes//:vid-id", uploadHandler)
+
+	router.GET("/testpage", testPageHandler)
+
+	return router
+}
+
+// 拦截
+func (m middleWareHander) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !m.l.GetConn() {
+		sendErrorResponse(w, http.StatusTooManyRequests, "Too many requests")
+		return
+	}
+
+	m.r.ServeHTTP(w, r)
+	defer m.l.ReleaseConn()
+}
+
+func main() {
+	r := RegisterHandlers()
+	m := NewMiddleWareHander(r, 2)
+	http.ListenAndServe("9000", m)
+
 }
